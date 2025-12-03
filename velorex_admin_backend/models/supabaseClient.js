c88f5bcee69f1b858d9993a7f361b44f5f143c92
sujal338864 <78285399+sujal338864@users.noreply.github.com>
2025-12-03 08:44:33 +0530

    Update supabaseClient.js

--- velorex_admin_backend/models/supabaseClient.js
+++ velorex_admin_backend/models/supabaseClient.js
@@ -1,11 +1,1 @@
 
-
-// // models/supabaseClient.js
-// const { createClient } = require("@supabase/supabase-js");
-
-// const SUPABASE_URL = "https://zyryndjeojrzvoubsqsg.supabase.co"; // your project URL
-// const SUPABASE_KEY = "YOUR_SERVICE_ROLE_KEY"; // ⚠️ from Supabase → Project → Settings → API
-
-// const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-
-// module.exports = supabase;

df2f99f6d818d5722ed0983f5ffcfd1c14a485ef
sujal338864 <sujal338864@gmail.com>
2025-12-17 15:59:58 +0530

    Deploy Flutter Web to GitHub Pages

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "cf20694145e4cbeb182523547283fe2b
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "b1970a8a896a285689db0ff0bf8ba38b",
+"flutter_bootstrap.js": "0b52cb2081c834c3c2895d024f40556d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
